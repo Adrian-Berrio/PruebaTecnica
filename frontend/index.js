7b987894346e1f3443a511bf8637dbee9da42996
Adrian-Berrio <adrianberriovahos@gmail.com>
2024-06-24 13:37:35 -0500

    Botones de edicion e historial

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,26 @@
 // button create
 const createBtn = document.querySelector('.create-btn');
 createBtn.addEventListener('click', () => {
+    const newItemText = prompt('Introduce el texto:');
 
-    // lo que hará el boton crear
-    addElementToList('gfdsf');
+    // do it button create
+    addElementToList(newItemText);
+    historial('se creó el elemento: ' + newItemText);
 });
 
 
 // list products or service
 const listContainer = document.querySelector('.list');
+const historialContainer = document.querySelector('.historial-list');
+
+
+function historial(action) {
+    const historialItem = document.createElement('div');
+    historialItem.classList.add('historial-item');
+    historialItem.textContent = action;
+    historialContainer.appendChild(historialItem);
+}
+
 
 function addElementToList(texto) {
     const newElement = document.createElement('div');
@@ -21,11 +33,28 @@ function addElementToList(texto) {
             <button class="desactivar-btn"><img src="../images/desactivar.svg" alt="delete"></button>
         </div>`;
 
-        
-
         listContainer.appendChild(newElement);
+
+        //TODO edit buttom
+        const editBtn = newElement.querySelector('.edit-btn');
+        editBtn.addEventListener('click', () => {
+            const newText = prompt('Edita el texto:', texto);
+        if (newText) {
+            newElement.querySelector('span').textContent = newText;
+        }
+
+        historial('se editó:' + texto);
+        texto = newText;
+
+        });
+
+        //TODO delete button
+        const desactivarBtn = newElement.querySelector('.desactivar-btn');
+        desactivarBtn.addEventListener('click', () => {
+            listContainer.removeChild(newElement);
+            historial ('se desactivó: ' + texto);
+        });
 }
 
-addElementToList('Hola prueba');
-addElementToList('prueba 2');
+
 
